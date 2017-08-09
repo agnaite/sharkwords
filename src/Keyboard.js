b1b494793a5ce9b1fcffc2eaa62eeee3795d30d5
Agne Klimaite <agnaite@me.com>
2017-08-08 17:25:11 -0700

    now disables button after click

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 class Keyboard extends Component {
+  selectLetter(evt) {
+    console.log(evt.target.innerText);
+    evt.target.disabled = true;
+    console.log(evt.target.disabled);
+  }
+
   render() {
     const letters = 'abcdefghijklmnopqrstuvw';
     const letterBtns = letters.split('').map((letter) =>
-      <button className='btn btn-default'>{ letter }</button>
+      <button className='btn btn-default' onClick={this.selectLetter}>
+        { letter }
+      </button>
     );
     return (
       <div className='container'>
